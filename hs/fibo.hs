21b96b126367eca41a947f5e79adcd0cef500929
MichaeGon <lodnix@gmail.com>
2016-06-09 16:20:35 +0900

    haskell

---
+++ hs/fibo.hs
@@ -0,0 +1,15 @@
+import Control.Monad
+
+main :: IO ()
+main = readLn >>= print . fibo
+
+fibo :: Integer -> Integer
+fibo x = last fibs
+    where
+        fibs = walk (0 : 1 : fibs) x
+
+walk :: [Integer] -> Integer -> [Integer]
+walk (x : y : zs) n
+    | n < 0 = []
+    | otherwise = x : walk (y : x + y : zs) (n - 1)
+walk _ _ = error "unreachable"
