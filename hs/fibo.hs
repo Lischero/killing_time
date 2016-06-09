17650f0f2bad6fdf3a7552dba302f21d62ac97be
MichaeGon <lodnix@gmail.com>
2016-06-09 16:39:08 +0900

    fix2

--- hs/fibo.hs
+++ hs/fibo.hs
@@ -4,12 +4,14 @@ main :: IO ()
 main = readLn >>= print . fibo
 
 fibo :: Integer -> Integer
-fibo x = last fibs
+fibo x
+    | x < 2 = x
+    | otherwise = last fibs
     where
-        fibs = 0 : 1 : walk fibs x
+        fibs = 0 : 1 : walk fibs (x - 1)
 
 walk :: [Integer] -> Integer -> [Integer]
 walk (x : y : zs) n
-    | n < 0 = []
-    | otherwise = x : walk (y : x + y : zs) (n - 1)
+    | n == 0 = []
+    | otherwise = (x + y) : walk (y : zs) (n - 1)
 walk _ _ = error "unreachable"
