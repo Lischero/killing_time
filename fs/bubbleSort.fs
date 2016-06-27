2981148880ad121a6771e2f77d59b08a0bf82ad6
MichaeGon <lodnix@gmail.com>
2016-06-27 21:15:45 +0900

    fs

---
+++ fs/bubbleSort.fs
@@ -0,0 +1,26 @@
+open System
+
+let mutable count = 0
+
+let rec innerBSort = function
+    | x :: y :: zs when x > y ->
+        count <- count + 1
+        let (res, _) = innerBSort (x :: zs)
+        (y :: res, true)
+    | x :: y :: zs ->
+        let (res, flag) = innerBSort (y :: zs)
+        (x :: res, flag)
+    | xs -> (xs, false)
+
+let rec bubbleSort xs =
+    match innerBSort xs with
+    | (res, true) -> bubbleSort res
+    | (res, _) -> res
+
+Console.ReadLine() |> ignore
+Array.map (int) (Console.ReadLine().Split(' '))
+    |> Array.toList
+    |> bubbleSort
+    |> List.iter (printf "%d ")
+printfn ""
+printfn "%d" count
