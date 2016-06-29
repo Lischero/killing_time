861e853848173e92d03fdd24c7468cb379dd3834
Lischero <vermouth.lischero@gmail.com>
2016-06-29 12:39:01 +0900

    add selectionSort.c

---
+++ c/selectionSort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void){
+	int min, N = 0;
+	scanf("%d",&N);
+	int list[N];
+	for(int count = 0; count < N; count++){
+		scanf("%d",&list[count]);
+	}
+	for(int i = 0; i < N-1; i++){
+		min = i;
+		for(int j = i+1; j < N; j++){
+			if(list[min] > list[j]){
+				min = j;
+			}
+		}
+		int tmp = list[i];
+		list[i] = list[min];
+		list[min] = tmp;
+	}
+	for(int count = 0; count < N; count++){
+		(count != N - 1) ? printf("%d ",list[count]) : printf("%d\n",list[count]);
+	}
+	return 0;
+}
